8724fc8e4d6a72c1c753f22e445b8cf6e3517961
DeltaStruct <DeltaStruct47@gmail.com>
2024-07-08 16:36:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 import time
 
 def connect_gspread(jsonf,key,name):
-  scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+  scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
   credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
   gc = gspread.authorize(credentials)
   SPREADSHEET_KEY = key
@@ -15,5 +15,10 @@ jsonf = "g.json"
 spread_sheet_key = "1zXDtkFmskO5NSxkqck8uDbcJtAhTVZtzPh2hLw64Sw4"
 ws = connect_gspread(jsonf,spread_sheet_key,"難易度表")
 for i in range(ws.row_count):
-  print(ws.cell(i+1,4).value)
+  task = ws.cell(i+1,4).value
+  vote = ws.cell(i+1,6).value
+  print("fetch: " + task)
+  print(vote)
+  f = open(task,'w')
+  write(vote)
   time.sleep(2)
