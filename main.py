3ba891396322fa7d897a56662379a31dba8358f8
DeltaStruct <DeltaStruct47@gmail.com>
2024-07-08 15:14:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import json
 from oauth2client.service_account import ServiceAccountCredentials
 
 # (1) Google Spread Sheetsにアクセス
-def connect_gspread(jsonf,key):
+def connect_gspread(jsonf,key,name):
     scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
     credentials = ServiceAccountCredentials.from_json_keyfile_name(jsonf, scope)
     gc = gspread.authorize(credentials)
     SPREADSHEET_KEY = key
-    worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet("難易度表")
+    worksheet = gc.open_by_key(SPREADSHEET_KEY).worksheet(name)
     return worksheet
 
 # ここでjsonfile名と2-2で用意したkeyを入力
 jsonf = "g.json"
 spread_sheet_key = "1zXDtkFmskO5NSxkqck8uDbcJtAhTVZtzPh2hLw64Sw4"
-ws = connect_gspread(jsonf,spread_sheet_key)
+ws = connect_gspread(jsonf,spread_sheet_key,"難易度表")
+print(ws.cell(1,4).value)
