6d260f7295572a574e4e722859b2d91fd01e200d
DeltaStruct <DeltaStruct47@gmail.com>
2024-07-08 16:39:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ ws = connect_gspread(jsonf,spread_sheet_key,"難易度表")
 for i in range(ws.row_count):
   task = ws.cell(i+1,4).value
   vote = ws.cell(i+1,6).value
+  if task==None:
+    sleep(2)
+    continue
   print("fetch: " + task)
   print(vote)
   f = open(task,'w')
