bd3c6706d44a353df74acc0a1a207ff4243e031f
DeltaStruct <DeltaStruct47@gmail.com>
2024-07-09 09:48:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,6 +20,34 @@ for i in range(ws.row_count):
     time.sleep(1)
     continue
   vote = ws.cell(i+1,6).value
+  if task not in "春合宿":
+    if task in "春":
+      task.replace("春","春合宿")
+    elif task in "合宿":
+      task.replace("合宿","春合宿")
+  if task in "JOIG-":
+    task.replace("JOIG-","JOIG")
+  print("fetch: " + task)
+  print(vote)
+  f = open(task,'w')
+  f.write(vote)
+  f.close()
+  time.sleep(2)
+
+ws = connect_gspread(jsonf,spread_sheet_key,"難易度表 New")
+for i in range(ws.row_count):
+  task = ws.cell(i+1,4).value
+  if task==None:
+    time.sleep(1)
+    continue
+  vote = ws.cell(i+1,6).value
+  if task not in "春合宿":
+    if task in "春":
+      task.replace("春","春合宿")
+    elif task in "合宿":
+      task.replace("合宿","春合宿")
+  if task in "JOIG-":
+    task.replace("JOIG-","JOIG")
   print("fetch: " + task)
   print(vote)
   f = open(task,'w')
